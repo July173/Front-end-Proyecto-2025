d48df9c0ba4964229c100b5089ef9ba05fc63b0e
July <danielitha345ra@gmail.com>
2025-09-17 15:40:22 -0500

    arreglos

--- src/hook/ValidationEmail.ts
+++ src/hook/ValidationEmail.ts
@@ -2,6 +2,7 @@
 export function useValidationEmail(email: string) {
 	const senaRegex = /^[a-zA-Z0-9._%+-]+@soy\.sena\.edu\.co$/;
 	const isValid = senaRegex.test(email);
-	const error = isValid ? '' : 'Solo se permiten correos @soy.sena.edu.co.';
+	const error = isValid ? '' : 'Solo se permiten correos @soy.sena.edu.co y @sena.edu.co';
 	return { isValid, error };
 }
+	
